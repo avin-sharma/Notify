fce204dc3eafb73ac0ac4535eda491a8add72fe2
quickavin <sharmaavin@hotmail.com>
2016-04-16 05:33:48 +0530

    Added Bottom Navigation Bar without icons

--- app/src/main/java/com/avinsharma/whatsnext/MainActivity.java
+++ app/src/main/java/com/avinsharma/whatsnext/MainActivity.java
@@ -1,13 +1,25 @@
 package com.avinsharma.whatsnext;
 
 import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnMenuTabClickListener;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomBar mBottomBar;
+
+
+    AccessTokenTracker accessTokenTracker;
+    AccessToken accessToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +27,79 @@ public class MainActivity extends AppCompatActivity {
         FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(this);
         setContentView(R.layout.activity_main);
+
+
+        //For Reference see this https://github.com/roughike/BottomBar
+
+        mBottomBar = BottomBar.attach(findViewById(R.id.activity_main), savedInstanceState);
+        mBottomBar.setItemsFromMenu(R.menu.bottombar_menu, new OnMenuTabClickListener() {
+            @Override
+            public void onMenuTabSelected(@IdRes int menuItemId) {
+                if (menuItemId == R.id.bottomBarItemOne) {
+                    // The user selected item number one.
+                    //TODO:Add icons and fragments/activities for switching between screens
+                }
+                /*if (menuItemId == R.id.bottomBarItemTwo) {
+                    // The user selected item number one.
+                    //TODO:Add icons and fragments/activities for switching between screens
+                }
+                if (menuItemId == R.id.bottomBarItemThree) {
+                    // The user selected item number one.
+                    //TODO:Add icons and fragments/activities for switching between screens
+                }
+                if (menuItemId == R.id.bottomBarItemFour) {
+                    // The user selected item number one.
+                    //TODO:Add icons and fragments/activities for switching between screens
+                }*/
+            }
+
+            @Override
+            public void onMenuTabReSelected(@IdRes int menuItemId) {
+                if (menuItemId == R.id.bottomBarItemOne) {
+                    // The user reselected item number one, scroll your content to top.
+                    //TODO:What to do when user clicks the item again like giving a toast message
+                }
+                if (menuItemId == R.id.bottomBarItemTwo) {
+                    // The user selected item number one.
+                    //TODO:What to do when user clicks the item again like giving a toast message
+                }
+                if (menuItemId == R.id.bottomBarItemThree) {
+                    // The user selected item number one.
+                    //TODO:What to do when user clicks the item again like giving a toast message
+                }
+                if (menuItemId == R.id.bottomBarItemFour) {
+                    // The user selected item number one.
+                    //TODO:What to do when user clicks the item again like giving a toast message
+                }
+            }
+        });
+
+        // Setting colors for different tabs when there's more than three of them.
+        // You can set colors for tabs in three different ways as shown below.
+        mBottomBar.mapColorForTab(0, ContextCompat.getColor(this, R.color.colorAccent));
+        mBottomBar.mapColorForTab(1, 0xFF5D4037);
+        mBottomBar.mapColorForTab(2, "#7B1FA2");
+        mBottomBar.mapColorForTab(3, "#FF5252");
+//        mBottomBar.mapColorForTab(4, "#FF9800");
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        // Necessary to restore the BottomBar's state, otherwise we would
+        // lose the current tab on orientation change.
+        mBottomBar.onSaveInstanceState(outState);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        accessTokenTracker.stopTracking();
+        super.onDestroy();
+    }
+
+    //TODO: OVERRIDE ON DESTROY FOR FACEBOOK ACCESS TOKEn
+
+
 }
