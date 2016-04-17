6ccbec5ea960995db54f1e01367a56ccb91e6e81
quickavin <sharmaavin@hotmail.com>
2016-04-17 20:45:17 +0530

    Added a temporary splash screen, update with new icon for the application

---
+++ app/src/main/java/com/avinsharma/whatsnext/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.avinsharma.whatsnext;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Avin on 17-04-2016.
+ */
+
+//For reference check this out: https://www.bignerdranch.com/blog/splash-screens-the-right-way/
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
